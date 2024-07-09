a3c1782fe7ad1f4d927f81c31a0b8359fa18f249
Angel Motta <angel.motta@utec.edu.pe>
2024-07-08 22:17:43 -0500

    feat: detail component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { HousingLocation } from "../housing-location";
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+    selector: "app-details",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./details.component.html",
+    styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
+    route: ActivatedRoute = inject(ActivatedRoute);
+    housingService = inject(HousingService);
+    housingDetail: HousingLocation | undefined;
 
+    constructor() {
+        const housingLocId = Number(this.route.snapshot.params["id"]);
+        this.housingDetail =
+            this.housingService.getHousingLocationById(housingLocId);
+    }
 }
